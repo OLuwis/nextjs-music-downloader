59c42e5170369a3246d7dc3f5480d0c06745647a
Luis Miguel <oluismrs@gmail.com>
2024-12-27 00:00:44 -0300

    initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+import Title from "@/app/components/home/Title";
+import Subtitle from "@/app/components/home/Subtitle";
+import Input from "@/app/components/common/Input";
+import BSolid from "@/app/components/common/buttons/BSolid";
+import BWhite from "./components/common/buttons/BWhite";
+
+export default function Home() {
+  return (
+    <div className="relative overflow-hidden">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
+        <div className="text-center">
+          <Title text="Music Downloader" />
+          <Subtitle text="Simple and free music downloader." />
+          <div className="mt-2 sm:mt-3 mx-auto max-w-xl relative">
+            <div className="relative z-10 flex gap-x-3 p-3">
+              <Input type="email" value="" placeholder="Paste your track url here." />
+              <div>
+                <BSolid leftIcon={<svg className="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>} />
+              </div>
+            </div>
+            <div className="hidden md:block absolute top-0 end-0 -translate-y-12 translate-x-12">
+              <svg className="w-16 h-auto text-orange-500" width="121" height="135" viewBox="0 0 121 135" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 16.4754C11.7688 27.4499 21.2452 57.3224 5 89.0164" stroke="currentColor" strokeWidth="10" strokeLinecap="round" />
+                <path d="M33.6761 112.104C44.6984 98.1239 74.2618 57.6776 83.4821 5" stroke="currentColor" strokeWidth="10" strokeLinecap="round" />
+                <path d="M50.5525 130C68.2064 127.495 110.731 117.541 116 78.0874" stroke="currentColor" strokeWidth="10" strokeLinecap="round" />
+              </svg>
+            </div>
+            <div className="hidden md:block absolute bottom-0 start-0 translate-y-10 -translate-x-36">
+              <svg className="w-40 h-auto text-cyan-500" width="347" height="188" viewBox="0 0 347 188" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 82.4591C54.7956 92.8751 30.9771 162.782 68.2065 181.385C112.642 203.59 127.943 78.57 122.161 25.5053C120.504 2.2376 93.4028 -8.11128 89.7468 25.5053C85.8633 61.2125 130.186 199.678 180.982 146.248L214.898 107.02C224.322 95.4118 242.9 79.2851 258.6 107.02C274.299 134.754 299.315 125.589 309.861 117.539L343 93.4426" stroke="currentColor" strokeWidth="7" strokeLinecap="round" />
+              </svg>
+            </div>
+          </div>
+          <div className="mt-2 sm:mt-3">
+            <BWhite leftIcon={<svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="14" x="2" y="7" rx="2" ry="2" /><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" /></svg>} text="Text" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+import Preline from "preline/plugin";
+import TailwindCSSForms from "@tailwindcss/forms"
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/preline/preline.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    TailwindCSSForms,
+    Preline,
+  ],
+} satisfies Config;
