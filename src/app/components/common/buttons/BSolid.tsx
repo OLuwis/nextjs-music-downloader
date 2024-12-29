656783d7ae8d653a75de7fd4279c1abb49244597
Luis Miguel <oluismrs@gmail.com>
2024-12-29 08:14:58 -0300

    add classname prop

--- src/app/components/common/buttons/BSolid.tsx
+++ src/app/components/common/buttons/BSolid.tsx
@@ -6,24 +6,26 @@ export default function BSolid({
 	isLink,
 	onClick,
 	leftIcon,
-	rightIcon
+	rightIcon,
+	className
 }: {
 	text?: string,
 	href?: string,
 	isLink?: boolean,
 	onClick?: MouseEventHandler<HTMLButtonElement>,
 	leftIcon?: ReactNode,
-	rightIcon?: ReactNode
+	rightIcon?: ReactNode,
+	className?: string
 }) {
 	return (
 		isLink ?
-			<a className="size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href={href}>
+			<a className={`size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none ${className}`} href={href}>
 				{leftIcon}
 				{text}
 				{rightIcon}
 			</a>
 			:
-			<button className="size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" onClick={onClick}>
+			<button className={`size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none ${className}`} onClick={onClick}>
 				{leftIcon}
 				{text}
 				{rightIcon}
