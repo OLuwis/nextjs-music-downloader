95b8e1ada43c58699926d0b899c56bd49b0fe4e6
Luis Miguel <oluismrs@gmail.com>
2024-12-29 00:09:30 -0300

    add input light mode styles

--- src/app/components/common/Input.tsx
+++ src/app/components/common/Input.tsx
@@ -15,7 +15,7 @@ export default function Input({
 }) {
 	return (
 		<div className="w-full">
-			<input type={type} name={name} className="peer py-2.5 px-4 block w-full border-transparent rounded-lg focus:invalid:border-red-500 dark:focus:invalid:border-red-500 focus:invalid:ring-red-500 dark:focus:invalid:ring-red-500 focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder={placeholder} value={value} onChange={onChange} />
+			<input type={type} name={name} className="peer py-2.5 px-4 block w-full border-transparent rounded-lg text-neutral-600 focus:invalid:border-red-500 dark:focus:invalid:border-red-500 focus:invalid:ring-red-500 dark:focus:invalid:ring-red-500 focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder={placeholder} value={value} onChange={onChange} />
 			<p className="peer-valid:hidden mt-2 pl-1.5 text-sm text-left text-red-500 dark:text-red-500">Track url should begin with - https://</p>
 		</div>
 	)
