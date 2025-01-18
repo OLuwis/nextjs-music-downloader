249a5d1031dc2d089efa390678db2b9c3f1def36
Luis Miguel <oluismrs@gmail.com>
2025-01-17 22:14:49 -0300

    update subtitle component parameters

--- src/app/components/home/Subtitle.tsx
+++ src/app/components/home/Subtitle.tsx
@@ -1,11 +1,11 @@
 export default function Subtitle({
 	text
 }: {
-	text: string
+	text: string;
 }) {
 	return (
 		<p className="mt-3 text-gray-600 dark:text-neutral-400">
 			{text}
 		</p>
-	)
+	);
 }
