8e09bb5317266caebf8829492f5958726e441578
Luis Miguel <oluismrs@gmail.com>
2025-01-17 22:15:31 -0300

    update title component parameters

--- src/app/components/home/Title.tsx
+++ src/app/components/home/Title.tsx
@@ -1,11 +1,11 @@
 export default function Title({
 	text
 }: {
-	text: string
+	text: string;
 }) {
 	return (
 		<h1 className="text-4xl sm:text-6xl font-bold text-gray-800 dark:text-neutral-200">
 			{text}
 		</h1>
-	)
+	);
 }
