1e8394b0318cbe319a2e015349fe1fafa3def65c
Luis Miguel <oluismrs@gmail.com>
2025-01-17 22:19:37 -0300

    update css import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import PrelineScript from "@/app/components/common/PrelineScript";
-import "./globals.css";
+import "@/app/globals.css";
 import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
