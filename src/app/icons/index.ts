13814545175a7845276fb1b3193db2f4cb91a156
Luis Miguel <oluismrs@gmail.com>
2025-01-17 22:17:40 -0300

    add icons components index exporter

---
+++ src/app/icons/index.ts
@@ -0,0 +1,8 @@
+import LoaderIcon from "@/app/icons/Loader";
+import GithubIcon from "@/app/icons/Github";
+import ArrowRightIcon from "@/app/icons/ArrowRight";
+import SunIcon from "@/app/icons/Sun";
+import MoonIcon from "@/app/icons/Moon";
+import DownloadIcon from "@/app/icons/Download";
+
+export { LoaderIcon, GithubIcon, ArrowRightIcon, SunIcon, MoonIcon, DownloadIcon };
