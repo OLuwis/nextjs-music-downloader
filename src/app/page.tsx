a788a63d90bea2fc7d05533bc54924b11859fcc6
Luis Miguel <oluismrs@gmail.com>
2025-01-17 22:20:47 -0300

    update forms and buttons on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-"use client"
+"use client";
 
 import Title from "@/app/components/home/Title";
 import Subtitle from "@/app/components/home/Subtitle";
 import Input from "@/app/components/common/Input";
-import BSolid from "@/app/components/common/buttons/BSolid";
-import BWhite from "./components/common/buttons/BWhite";
+import ButtonSolid from "@/app/components/common/buttons/Solid";
+import ButtonWhite from "@/app/components/common/buttons/White";
+import { LoaderIcon, ArrowRightIcon, GithubIcon, SunIcon, MoonIcon } from "@/app/icons";
 import { useState } from "react";
 import { useTheme } from "next-themes";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [inputValue, setInputValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   return (
     <div className="relative overflow-hidden">
@@ -22,14 +23,14 @@ export default function Home() {
           </a>
           <Subtitle text="Simple and free music downloader." />
           <div className="mt-2 sm:mt-3 mx-auto max-w-xl relative">
-            <form action="/download" method="GET" className="relative z-10 flex gap-x-3 p-3" onSubmit={() => setIsLoading(true)} >
-              <Input type="url" value={inputValue} placeholder="Paste your track url here." onChange={(e) => setInputValue(e.target.value)} />
+            <form action="/song" method="GET" className="relative z-10 flex gap-x-3 p-3" onSubmit={() => setIsLoading(true)} >
+              <Input type="url" name="url" value={inputValue} placeholder="Paste your track url here." onChange={(e) => setInputValue(e.target.value)} />
               <div>
-                <BSolid leftIcon={
+                <ButtonSolid leftIcon={
                   isLoading ?
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-loader-circle animate-spin"><path d="M21 12a9 9 0 1 1-6.219-8.56" /></svg>
+                    <LoaderIcon className="animate-spin" />
                     :
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg>
+                    <ArrowRightIcon />
                 } />
               </div>
             </form>
@@ -49,14 +50,15 @@ export default function Home() {
           <div className="mt-2 sm:mt-3">
             {
               theme === "dark" ?
-                <BWhite leftIcon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" /></svg>} text="Dark Mode" onClick={() => setTheme("light")} />
+                <ButtonWhite leftIcon={<MoonIcon />} text="Dark Mode" onClick={() => setTheme("light")}
+                />
                 :
-                <BWhite leftIcon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sun"><circle cx="12" cy="12" r="4" /><path d="M12 2v2" /><path d="M12 20v2" /><path d="m4.93 4.93 1.41 1.41" /><path d="m17.66 17.66 1.41 1.41" /><path d="M2 12h2" /><path d="M20 12h2" /><path d="m6.34 17.66-1.41 1.41" /><path d="m19.07 4.93-1.41 1.41" /></svg>} text="Light Mode" onClick={() => setTheme("dark")} />
+                <ButtonWhite leftIcon={<SunIcon />} text="Light Mode" onClick={() => setTheme("dark")} />
             }
-            <BWhite leftIcon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>} text="Github" isLink href="#" />
+            <ButtonWhite leftIcon={<GithubIcon />} text="Github" isLink href="#" />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
