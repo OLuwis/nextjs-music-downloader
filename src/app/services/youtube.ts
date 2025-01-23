19b968a11288ac07e7f05024ac13ffcc6a6b3285
Luis Miguel <oluismrs@gmail.com>
2025-01-23 05:13:23 -0300

    log testing #9

--- src/app/services/youtube.ts
+++ src/app/services/youtube.ts
@@ -1,34 +1,69 @@
 import Innertube from "youtubei.js";
 import Song from "../types/song";
+import { JSDOM } from "jsdom";
 import { stdout } from "process";
+import BG, { BgConfig } from "bgutils-js";
 
 // Return new instance of Youtube API
-export function createYoutube(_visitorData?: string): Promise<Innertube> {
-	const visitorData = _visitorData ?? "";
+export async function createYoutube(): Promise<Innertube> {
+	const innertube = await Innertube.create({ retrieve_player: false });
 
-	return Innertube.create({ visitor_data: visitorData });
+	const requestKey = "O43z0dpjhgX20SCx4KAo";
+	const visitorData = innertube.session.context.client.visitorData;
+
+	if (!visitorData) {
+		throw new Error("No visitor data!");
+	}
+
+	const dom = new JSDOM();
+
+	Object.assign(globalThis, {
+		window: dom.window,
+		document: dom.window.document
+	});
+
+	const bgConfig: BgConfig = {
+		fetch: (input: string | URL | globalThis.Request, init?: RequestInit) => fetch(input, init),
+		globalObj: globalThis,
+		identifier: visitorData,
+		requestKey
+	};
+
+	const bgChallenge = await BG.Challenge.create(bgConfig);
+
+	if (!bgChallenge)
+		throw new Error('Could not get challenge');
+
+	const interpreterJavascript = bgChallenge.interpreterJavascript.privateDoNotAccessOrElseSafeScriptWrappedValue;
+
+	if (interpreterJavascript) {
+		new Function(interpreterJavascript)();
+	} else throw new Error('Could not load VM');
+
+	const poTokenResult = await BG.PoToken.generate({
+		program: bgChallenge.program,
+		globalName: bgChallenge.globalName,
+		bgConfig
+	});
+
+	const placeholderPoToken = BG.PoToken.generatePlaceholder(visitorData);
+
+	stdout.write(`Session Info: {
+		${visitorData},
+		${placeholderPoToken},
+		poToken: ${poTokenResult.poToken},
+		integrityTokenData: ${poTokenResult.integrityTokenData}
+	}`);
+
+	return Innertube.create({
+		po_token: poTokenResult.poToken,
+		visitor_data: visitorData
+	});
 }
 
 // Return track data from url
 export async function fetchTrackData(_url: string, _youtube?: Innertube): Promise<Song> {
-	const res = await fetch("https://music.youtube.com", {
-		headers: {
-			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-		}
-	})
-
-	const page = await res.text();
-
-	stdout.write(page);
-
-	const firstIndex = page.indexOf("visitorData");
-	const secondIndex = page.indexOf("userAgent", firstIndex);
-
-	const visitorData = page.substring(firstIndex + "visitorData\":\"".length, secondIndex - "\",\"".length);
-
-	stdout.write(visitorData);
-
-	const youtube = _youtube ?? await createYoutube(visitorData);
+	const youtube = _youtube ?? await createYoutube();
 
 	const url = new URL(_url);
 	stdout.write(`url => ${url.toString()}`)
