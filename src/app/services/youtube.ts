6532b382c776a5f033a29108297cb9ea2282a085
Luis Miguel <oluismrs@gmail.com>
2025-01-23 05:45:36 -0300

    log testing #11

--- src/app/services/youtube.ts
+++ src/app/services/youtube.ts
@@ -1,64 +1,10 @@
-import Innertube from "youtubei.js";
+import Innertube, { ClientType } from "youtubei.js";
 import Song from "../types/song";
-import { JSDOM } from "jsdom";
 import { stdout } from "process";
-import BG, { BgConfig } from "bgutils-js";
 
 // Return new instance of Youtube API
 export async function createYoutube(): Promise<Innertube> {
-	const innertube = await Innertube.create({ retrieve_player: false });
-
-	const requestKey = "O43z0dpjhgX20SCx4KAo";
-	const visitorData = innertube.session.context.client.visitorData;
-
-	if (!visitorData) {
-		throw new Error("No visitor data!");
-	}
-
-	const dom = new JSDOM();
-
-	Object.assign(globalThis, {
-		window: dom.window,
-		document: dom.window.document
-	});
-
-	const bgConfig: BgConfig = {
-		fetch: (input: string | URL | globalThis.Request, init?: RequestInit) => fetch(input, init),
-		globalObj: globalThis,
-		identifier: visitorData,
-		requestKey
-	};
-
-	const bgChallenge = await BG.Challenge.create(bgConfig);
-
-	if (!bgChallenge)
-		throw new Error('Could not get challenge');
-
-	const interpreterJavascript = bgChallenge.interpreterJavascript.privateDoNotAccessOrElseSafeScriptWrappedValue;
-
-	if (interpreterJavascript) {
-		new Function(interpreterJavascript)();
-	} else throw new Error('Could not load VM');
-
-	const poTokenResult = await BG.PoToken.generate({
-		program: bgChallenge.program,
-		globalName: bgChallenge.globalName,
-		bgConfig
-	});
-
-	const placeholderPoToken = BG.PoToken.generatePlaceholder(visitorData);
-
-	stdout.write(`Session Info: {
-		${visitorData},
-		${placeholderPoToken},
-		poToken: ${poTokenResult.poToken},
-		integrityTokenData: ${poTokenResult.integrityTokenData}
-	}`);
-
-	return Innertube.create({
-		po_token: poTokenResult.poToken,
-		visitor_data: visitorData
-	});
+	return Innertube.create({ client_type: ClientType.ANDROID });
 }
 
 // Return track data from url
