bc188d0a11598ca159e59777bae156680560035b
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:16:54 -0300

    add global component import

---
+++ src/app/components/index.ts
@@ -0,0 +1,9 @@
+import White from "@/app/components/common/buttons/White";
+import Solid from "@/app/components/common/buttons/Solid";
+import Input from "@/app/components/common/Input";
+import Chip from "@/app/components/home/Chip";
+import Subtitle from "@/app/components/home/Subtitle";
+import Title from "@/app/components/home/Title";
+import Card from "@/app/components/song/Card";
+
+export { White as ButtonWhite, Solid as ButtonSolid, Input, Chip, Subtitle, Title, Card as SongCard }
