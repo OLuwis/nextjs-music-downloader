e10b5f4c109343914efa6e7946e18a91b31e06f5
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:26:04 -0300

    change global icon import

--- src/app/icons/index.ts
+++ src/app/icons/index.ts
@@ -4,5 +4,7 @@ import ArrowRightIcon from "@/app/icons/ArrowRight";
 import SunIcon from "@/app/icons/Sun";
 import MoonIcon from "@/app/icons/Moon";
 import DownloadIcon from "@/app/icons/Download";
+import SoundCloud from "@/app/icons/SoundCloud";
+import YoutubeMusic from "@/app/icons/YoutubeMusic";
 
-export { LoaderIcon, GithubIcon, ArrowRightIcon, SunIcon, MoonIcon, DownloadIcon };
+export { LoaderIcon, GithubIcon, ArrowRightIcon, SunIcon, MoonIcon, DownloadIcon, SoundCloud, YoutubeMusic };
