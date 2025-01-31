f2de246c969518f0a737f8579d9f4ff996b5c07e
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:14:03 -0300

    change ButtonWhite styles

--- src/app/components/common/buttons/White.tsx
+++ src/app/components/common/buttons/White.tsx
@@ -3,6 +3,7 @@ import { MouseEventHandler, ReactNode } from "react";
 export default function ButtonWhite({
 	text,
 	href,
+	size,
 	isLink,
 	onClick,
 	leftIcon,
@@ -11,6 +12,7 @@ export default function ButtonWhite({
 }: {
 	text?: string;
 	href?: string;
+	size?: "sm";
 	isLink?: boolean;
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 	leftIcon?: ReactNode;
@@ -19,13 +21,13 @@ export default function ButtonWhite({
 }) {
 	return (
 		isLink ?
-			<a className={`m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 ${className}`} href={href}>
+			<a className={`${size ? "size-[30px] w-fit" : "size-[46px]"} m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 ${className}`} href={href}>
 				{leftIcon}
 				{text}
 				{rightIcon}
 			</a>
 			:
-			<button className={`m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 ${className}`} onClick={onClick}>
+			<button className={`${size ? "size-[30px] w-fit" : "size-[46px]"} m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 ${className}`} onClick={onClick}>
 				{leftIcon}
 				{text}
 				{rightIcon}
