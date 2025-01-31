99c5e27d14f44562a20cba270924cd2e365a8e75
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:38:48 -0300

    change Buttons anchor target

--- src/app/components/common/buttons/Solid.tsx
+++ src/app/components/common/buttons/Solid.tsx
@@ -21,7 +21,7 @@ export default function ButtonSolid({
 }) {
 	return (
 		isLink ?
-			<a className={`${size ? "size-[30px] w-fit" : "size-[46px]"} inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none ${className}`} href={href} rel="_blank">
+			<a className={`${size ? "size-[30px] w-fit" : "size-[46px]"} inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none ${className}`} href={href} rel="_blank" target="_blank" rel="noopener noreferrer">
 				{leftIcon}
 				{text}
 				{rightIcon}
--- src/app/components/common/buttons/White.tsx
+++ src/app/components/common/buttons/White.tsx
@@ -21,7 +21,7 @@ export default function ButtonWhite({
 }) {
 	return (
 		isLink ?
-			<a className={`${size ? "size-[30px] w-fit" : "size-[46px]"} m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 ${className}`} href={href}>
+			<a className={`${size ? "size-[30px] w-fit" : "size-[46px]"} m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 ${className}`} href={href} target="_blank" rel="noopener noreferrer">
 				{leftIcon}
 				{text}
 				{rightIcon}
