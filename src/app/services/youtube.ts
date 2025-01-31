df5965c7113126d6bd5966f8bda45b35ee39ca06
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:34:41 -0300

    remove Youtube service debug logging

--- src/app/services/youtube.ts
+++ src/app/services/youtube.ts
@@ -1,10 +1,9 @@
-import Innertube, { ClientType } from "youtubei.js";
+import Innertube from "youtubei.js";
 import Song from "../types/song";
-import { stdout } from "process";
 
 // Return new instance of Youtube API
 export async function createYoutube(): Promise<Innertube> {
-	return Innertube.create({ client_type: ClientType.ANDROID });
+	return Innertube.create();
 }
 
 // Return track data from url
@@ -12,24 +11,17 @@ export async function fetchTrackData(_url: string, _youtube?: Innertube): Promis
 	const youtube = _youtube ?? await createYoutube();
 
 	const url = new URL(_url);
-	stdout.write(`url => ${url.toString()}`)
 
 	const id = url.searchParams.get("v") || "";
-	stdout.write(`id => ${id}`)
 
 	const info = await youtube.music.getInfo(id);
-	stdout.write(`info => ${JSON.stringify(info)}`)
 
 	const author = info.basic_info.author || "";
 	const title = info.basic_info.title || "";
 	const cover = info.basic_info.thumbnail ? info.basic_info.thumbnail[0].url : "";
-	stdout.write(`author => ${author}`)
-	stdout.write(`title => ${title}`)
-	stdout.write(`cover => ${cover}`)
 
 	const streamUrl = info.chooseFormat({ type: "video+audio" }).decipher(youtube.session.player);
 
-	stdout.write("Step 7")
 	return {
 		platform: "youtube music",
 		title: title,
@@ -45,11 +37,3 @@ export async function fetchTrackData(_url: string, _youtube?: Innertube): Promis
 		}
 	}
 }
-
-/* export async function authenticate(_youtube?: Innertube): Promise<Innertube> {
-	const youtube = _youtube ?? await createYoutube();
-
-	await youtube.session.signIn();
-
-	return youtube;
-} */
