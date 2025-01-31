d63c4acdff82b259ca7a4b9b9a240d36d56184c8
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:35:25 -0300

    add Song page logging and error handling

--- src/app/song/page.tsx
+++ src/app/song/page.tsx
@@ -1,30 +1,53 @@
 import SongCard from "@/app/components/song/Card";
-import Title from "@/app/components/home/Title";
+import { Title, Subtitle } from "@/app/components";
 import { fetchTrackData as soundCloudFetchTrackData } from "@/app/services/soundcloud";
-import { fetchTrackData as youtubeMusicFetchTrackData } from "@/app/services/youtube";
+import log from "loglevel";
 
 export default async function Page({
   searchParams
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const url = (await searchParams).url;
+  const params = await searchParams;
+  log.debug(`SearchParams: ${params}`);
 
-  const data = url!.includes("soundcloud") ? await soundCloudFetchTrackData(url as string) : await youtubeMusicFetchTrackData(url as string);
+  const url = params.url;
+  log.debug(`UrlParam: ${url}`);
 
-  return (
-    <>
-      <div className="relative overflow-hidden">
-        <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
-          <div className="text-center">
-            <a href=".">
-              <Title text="Music Downloader" />
-            </a>
+  const data = url!.includes("soundcloud") ? await soundCloudFetchTrackData(url as string) : null;
+  log.debug(`Data: ${data}`);
+
+  return data ?
+    (
+      <>
+        <div className="relative overflow-hidden">
+          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
+            <div className="text-center">
+              <a href=".">
+                <Title text="Music Downloader" />
+              </a>
+            </div>
+            <span className="block h-10 sm:h-20 aspect-square"></span>
+            <SongCard data={data} />
           </div>
-          <span className="block h-10 sm:h-20 aspect-square"></span>
-          <SongCard data={data} />
         </div>
-      </div>
-    </>
-  );
+      </>
+    )
+    :
+    (
+      <>
+        <div className="relative overflow-hidden">
+          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
+            <div className="text-center">
+              <a href=".">
+                <Title text="Music Downloader" />
+              </a>
+            </div>
+            <div className="text-center">
+              <Subtitle text="Invalid or unknown url." />
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
