bf56033eda628297873808216a68f388fb471a19
Luis Miguel <oluismrs@gmail.com>
2025-01-30 21:27:51 -0300

    add Layout logging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,21 @@ import { Inter } from "next/font/google";
 import PrelineScript from "@/app/components/common/PrelineScript";
 import "@/app/globals.css";
 import { ThemeProvider } from "next-themes";
+import log from "loglevel";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
+log.info(`FontType: ${inter}`);
+
 export const metadata: Metadata = {
   title: "Music Downloader",
   description: "Simple and free music downloader.",
 };
+
+log.info(`Metadata: ${metadata}`)
 
 export default function RootLayout({
   children,
